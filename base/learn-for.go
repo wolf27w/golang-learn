0c334f3973d1d559ff103ce5bb2100254afad8af
wolf <wolf27w@gmail.com>
2021-05-18 13:31:10 +0800

    for循环

--- base/learn-for.go
+++ base/learn-for.go
@@ -65,3 +65,28 @@ package main
 //		fmt.Printf("第 %d 位 x 的值 = %d\n", i,x)
 //	}
 //}
+
+//嵌套循环语法：
+//for [condition |  ( init; condition; increment ) | Range]
+//{
+//	for [condition |  ( init; condition; increment ) | Range]
+//	{
+//	statement(s)
+//	}
+//	statement(s)
+//}
+//10以内的所有素数
+//func main() {
+//	/* 定义局部变量 */
+//	var i, j int
+//	for i=2; i < 100; i++ {
+//		for j=2; j <= (i/j); j++ {
+//			if(i%j==0) {
+//				break // 如果发现因子，则不是素数
+//			}
+//		}
+//		if(j > (i/j)) {
+//			fmt.Printf("%d  是素数\n", i)
+//		}
+//	}
+//}
