850ae6cd946fa3a3c705645c4bbd21a3e7b9f292
wolf <wolf27w@gmail.com>
2021-05-18 17:39:34 +0800

    golang类型

---
+++ base/learn-type.go
@@ -0,0 +1,5 @@
+package main
+//golang没有"类"的概念，也不支持"类"的继承等面向对象的概念，golang中通过结构体的内嵌在配合接口比面向对象具有更高的扩展性和灵活性。
+//golan中，基本的数据类型，如string，整型，浮点型，布尔等数据类型，可以使用type关键字来定义类型
+//将MyInt定义为int类型
+//type MyInt int
