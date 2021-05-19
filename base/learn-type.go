c3ee26a1f3bc4d7c8c918835aae297531b03b894
wolf <wolf27w@gmail.com>
2021-05-19 18:30:05 +0800

    golang结构体

--- base/learn-type.go
+++ base/learn-type.go
@@ -46,11 +46,11 @@ package main
 
 
 
-type person struct {
-	name string
-	city string
-	age  int8
-}
+//type person struct {
+//	name string
+//	city string
+//	age  int8
+//}
 //func main() {
 //	var p1 person
 //	p1.name = "pprof.cn"    //通过.来访问结构体的字段（成员变量）
@@ -100,6 +100,69 @@ type person struct {
 //	fmt.Printf("p4=%#v\n", p4) //p4=main.person{name:"", city:"", age:0}
 //}
 
+
+//func main() {
+//	var p4 person
+//	fmt.Printf("p4=%#v\n", p4) //p4=main.person{name:"", city:"", age:0}
+//	p5 := person{    //键值对对结构体进行初始化时，键对应结构体的字段，值对应该字段的初始值。
+//		name: "pprof.cn",
+//		city: "北京",
+//		age:  18,
+//	}
+//	fmt.Printf("p5=%#v\n", p5) //p5=main.person{name:"pprof.cn", city:"北京", age:18}
+//	p6 := &person{     //对结构体指针进行键值对初始化
+//		name: "pprof.cn",
+//		city: "北京",
+//		age:  18,
+//	}
+//	fmt.Printf("p6=%#v\n", p6) //p6=&main.person{name:"pprof.cn", city:"北京", age:18}
+//	p7 := &person{    //当某些字段没有初始值的时候，该字段可以不写。此时，没有指定初始值的字段的值就是该字段类型的零值
+//		city: "北京",
+//	}
+//	fmt.Printf("p7=%#v\n", p7) //p7=&main.person{name:"", city:"北京", age:0}
+//	p8 := &person{     //初始化结构体的时候可以简写，也就是初始化的时候不写键
+//		"pprof.cn",
+//		"北京",
+//		18,
+//	}
+//	fmt.Printf("p8=%#v\n", p8) //p8=&main.person{name:"pprof.cn", city:"北京", age:18}
+//}
+//注意：
+//1.必须初始化结构体的所有字段。
+//2.初始值的填充顺序必须与字段在结构体中的声明顺序一致。
+//3.该方式不能和键值初始化方式混用。
+
+
+//###################结构体内存布局
+//type test struct {
+//	a int8
+//	b int8
+//	c int8
+//	d int8
+//}
+//
+//func main()  {
+//	type test struct {
+//		a int8
+//		b int8
+//		c int8
+//		d int8
+//	}
+//	n := test{
+//		1, 2, 3, 4,
+//	}
+//	fmt.Printf("n.a %p\n", &n.a)
+//	fmt.Printf("n.b %p\n", &n.b)
+//	fmt.Printf("n.c %p\n", &n.c)
+//	fmt.Printf("n.d %p\n", &n.d)
+//}
+//输出结果：
+//n.a 0xc000096004
+//n.b 0xc000096005
+//n.c 0xc000096006
+//n.d 0xc000096007
+
+//#######面试题
 //type student struct {
 //	name string
 //	age  int
@@ -114,14 +177,17 @@ type person struct {
 //	}
 //
 //	for _, stu := range stus {
-//		m[stu.name] = &stu
+//		m[stu.name] = &stu   //for循环后m.name最后一个值是博客
 //	}
-//	for k, v := range m {
+//	for k, v := range m {   //m的值是map的stus
 //		fmt.Println(k, "=>", v.name)
 //	}
 //}
 
-
+//输出结果：
+//pprof.cn => wolf
+//测试 => wolf
+//wolf => wolf
 
 
 
