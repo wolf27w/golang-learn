503c00cfb5bce8e370b17a30b6e41447e22b0ea0
wolf <wolf27w@gmail.com>
2021-05-24 15:19:22 +0800

    golang函数

--- base/learn-func.go
+++ base/learn-func.go
@@ -536,3 +536,90 @@ package main
 //b  closed
 //a  closed
 //defer后面的语句在执行的时候，函数调用的参数会被保存起来，但是不执行。也就是复制了一份。但是并没有说struct这里的this指针如何处理，通过这个例子可以看出go语言并没有把这个明确写出来的this指针当作参数来看待
+
+
+
+//多个 defer 注册，按 FILO 次序执行 ( 先进后出 )。哪怕函数或某个延迟调用发生错误，这些调用依旧会被执行。
+
+//func test(x int) {
+//	defer println("a")
+//	defer println("b")
+//
+//	defer func() {
+//		println(100 / x) // div0 异常未被捕获，逐步往外传递，最终终止进程。
+//	}()
+//
+//	defer println("c")
+//}
+//
+//func main() {
+//	test(0)
+//}
+//输出结果
+//c
+//b
+//a
+//panic: runtime error: integer divide by zero
+
+//*延迟调用参数在注册时求值或复制，可用指针或闭包 "延迟" 读取。
+
+
+//func test() {
+//	x, y := 10, 20
+//
+//	defer func(i int) {
+//		println("defer:", i, y) // y 闭包引用
+//	}(x) // x 被复制
+//
+//	x += 10
+//	y += 100
+//	println("x =", x, "y =", y)
+//}
+//
+//func main() {
+//	test()
+//}
+
+//输出结果
+//x = 20 y = 120
+//defer: 10 120
+
+
+//*滥用 defer 可能会导致性能问题，尤其是在一个 "大循环" 里。
+//var lock sync.Mutex
+//
+//func test() {
+//	lock.Lock()
+//	lock.Unlock()
+//}
+//
+//func testdefer() {
+//	lock.Lock()
+//	defer lock.Unlock()
+//}
+//
+//func main() {
+//	func() {
+//		t1 := time.Now()
+//
+//		for i := 0; i < 10000; i++ {
+//			test()
+//		}
+//		elapsed := time.Since(t1)
+//		fmt.Println("test elapsed: ", elapsed)
+//	}()
+//	func() {
+//		t1 := time.Now()
+//
+//		for i := 0; i < 10000; i++ {
+//			testdefer()
+//		}
+//		elapsed := time.Since(t1)
+//		fmt.Println("testdefer elapsed: ", elapsed)
+//	}()
+//
+//}
+//输出结果
+
+//test elapsed:  142.132µs
+//testdefer elapsed:  147.136µ
