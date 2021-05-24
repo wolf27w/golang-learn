b0ccad3d1170dcb9e9b914a45de1aebf1d63e882
wolf <wolf27w@gmail.com>
2021-05-24 15:24:22 +0800

    golang函数

--- base/learn-func.go
+++ base/learn-func.go
@@ -626,7 +626,29 @@ package main
 
 
 
+//############defer陷阱
 
+//func foo(a, b int) (i int, err error) {
+//	defer fmt.Printf("first defer err %v\n", err)
+//	defer func(err error) { fmt.Printf("second defer err %v\n", err) }(err)
+//	defer func() { fmt.Printf("third defer err %v\n", err) }()
+//	if b == 0 {
+//		err = errors.New("divided by zero!")
+//		return
+//	}
+//
+//	i = a / b
+//	return
+//}
+//
+//func main() {
+//	foo(2, 0)
+//}
+//输出结果
+//third defer err divided by zero!
+//second defer err <nil>
+//first defer err <nil>
+//解释：如果 defer 后面跟的不是一个 closure 最后执行的时候我们得到的并不是最新的值。
 
 
 
