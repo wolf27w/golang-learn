f7351f153e0f2b7d13b8f84296eed02b51805324
wolf <wolf27w@gmail.com>
2021-05-25 10:52:29 +0800

    golang函数

--- base/learn-func.go
+++ base/learn-func.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //函数的特点
 //• 无需声明原型。
 //• 支持不定 变参。
@@ -959,6 +961,26 @@ package main
 //}
 //输出结果
 //send on closed channel
+
+//延迟调用中引发的错误，可被后续延迟调用捕获，但仅最后一个错误可被捕获。
+
+//func test() {
+//	defer func() {
+//		fmt.Println(recover())
+//	}()
+//
+//	defer func() {
+//		panic("defer panic")
+//	}()
+//
+//	panic("test panic")
+//}
+//
+//func main() {
+//	test()
+//}
+//输出结果
+//defer panic
 
 
 
