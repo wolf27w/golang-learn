df7ff12e0847ed9a79846602d8503985fdaae1bd
wolf <wolf27w@gmail.com>
2021-05-25 10:57:20 +0800

    golang函数

--- base/learn-func.go
+++ base/learn-func.go
@@ -1058,8 +1058,31 @@ package main
 
 //标准库 errors.New 和 fmt.Errorf 函数用于创建实现 error 接口的错误对象。通过判断错误对象实例来确定具体错误类型。
 
+//var ErrDivByZero = errors.New("division by zero")
+//
+//func div(x, y int) (int, error) {
+//	if y == 0 {
+//		return 0, ErrDivByZero
+//	}
+//	return x / y, nil
+//}
+//
+//func main() {
+//	defer func() {
+//		fmt.Println(recover())
+//	}()
+//	switch z, err := div(10, 0); err {
+//	case nil:
+//		println(z)
+//	case ErrDivByZero:
+//		panic(err)
+//	}
+//}
+//输出结果
+//division by zero
 
 
+//Go实现类似 try catch 的异常处理
 
 
 
