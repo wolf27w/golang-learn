799c162072bc8812c6fbce54f68fe50cc29f8d0a
wolf <wolf27w@gmail.com>
2021-05-25 10:53:18 +0800

    golang函数

--- base/learn-func.go
+++ base/learn-func.go
@@ -982,11 +982,32 @@ import "fmt"
 //输出结果
 //defer panic
 
+//捕获函数 recover 只有在延迟调用内直接调用才会终止错误，否则总是返回 nil。任何未捕获的错误都会沿调用堆栈向外传递。
 
+func test() {
+	defer func() {
+		fmt.Println(recover()) //有效
+	}()
+	defer recover()              //无效！
+	defer fmt.Println(recover()) //无效！
+	defer func() {
+		func() {
+			println("defer inner")
+			recover() //无效！
+		}()
+	}()
 
+	panic("test panic")
+}
 
+func main() {
+	test()
+}
 
-
+//输出结果
+//defer inner
+//<nil>
+//test panic
 
 
 
