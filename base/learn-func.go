9a4296db579f1adb9373854f5310e00b091689a8
wolf <wolf27w@gmail.com>
2021-05-26 11:17:49 +0800

    golang函数

--- base/learn-func.go
+++ base/learn-func.go
@@ -1224,9 +1224,28 @@ package main
 //在当前目录下，我们创建一个split_test.go的测试文件，并定义一个测试函数如下：
 
 
+/// split/split_test.go
+//
+//package split
+//
+//import (
+//"reflect"
+//"testing"
+//)
+//
+//func TestSplit(t *testing.T) { // 测试函数名必须以Test开头，必须接收一个*testing.T类型参数
+//	got := Split("a:b:c", ":")         // 程序输出的结果
+//	want := []string{"a", "b", "c"}    // 期望的结果
+//	if !reflect.DeepEqual(want, got) { // 因为slice不能比较直接，借助反射包中的方法比较
+//		t.Errorf("excepted:%v, got:%v", want, got) // 测试失败输出错误提示
+//	}
+//}
 
-
-
+//此时包中u如下
+//split $ ls -l
+//total 16
+//-rw-r--r--  1 pprof staff  408  4 29 15:50 split.go
+//-rw-r--r--  1 pprof  staff  466  4 29 16:04 split_test.go
 
 
 
