92797f55a8e7ab5e117b712523700fd3b2daccaa
wolf <wolf27w@gmail.com>
2021-05-26 11:16:46 +0800

    golang函数

--- base/learn-func.go
+++ base/learn-func.go
@@ -1205,9 +1205,23 @@ package main
 
 
 
+// split package with a single split function.
 
+// Split slices s into all substrings separated by sep and
+// returns a slice of the substrings between those separators.
+//func Split(s, sep string) (result []string) {
+//	i := strings.Index(s, sep)
+//
+//	for i > -1 {
+//		result = append(result, s[:i])
+//		s = s[i+1:]
+//		i = strings.Index(s, sep)
+//	}
+//	result = append(result, s)
+//	return
+//}
 
-
+//在当前目录下，我们创建一个split_test.go的测试文件，并定义一个测试函数如下：
 
 
 
