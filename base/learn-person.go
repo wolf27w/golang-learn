39f443df1eb5830dbe0da60775186f680785c349
wolf <wolf27w@gmail.com>
2021-05-28 13:30:15 +0800

    golang面对对象

--- base/learn-person.go
+++ base/learn-person.go
@@ -96,10 +96,34 @@ package main
 //{{5lmh man 18} 1 bj}
 
 
+//指针类型匿名字段
 
+//人
+//type Person struct {
+//	name string
+//	sex  string
+//	age  int
+//}
+//
+//// 学生
+//type Student struct {
+//	*Person
+//	id   int
+//	addr string
+//}
+//
+//func main() {
+//	s1 := Student{&Person{"5lmh", "man", 18}, 1, "bj"}
+//	fmt.Println(s1)
+//	fmt.Println(s1.name)
+//	fmt.Println(s1.Person.name)
+//}
+//输出结果
+//{0xc000074180 1 bj}
+//5lmh
+//5lmh
 
-
-
+//########################接口
 
 
 
