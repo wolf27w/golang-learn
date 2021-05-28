43ba2598cae3f8fec8c6e832185b9d750e55f8ea
wolf <wolf27w@gmail.com>
2021-05-28 11:27:35 +0800

    golang方法

--- base/learn-test.go
+++ base/learn-test.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 //############方法定义
 
 //只能为当前包内命名类型定义方法。
@@ -379,31 +377,68 @@ import "fmt"
 //
 //这条规则说的是当我们嵌入一个类型，嵌入类型的接受者为值类型的方法将被提升，可以被外部类型的值和指针调用。
 
-type S struct {
-	T
-}
+//type S struct {
+//	T
+//}
+//
+//type T struct {
+//	int
+//}
+//
+//func (t T) testT() {
+//	fmt.Println("如类型 S 包含匿名字段 T，则 S 和 *S 方法集包含 T 方法。")
+//}
+//
+//func main() {
+//	s1 := S{T{1}}
+//	s2 := &s1
+//	fmt.Printf("s1 is : %v\n", s1)
+//	s1.testT()
+//	fmt.Printf("s2 is : %v\n", s2)
+//	s2.testT()
+//}
+//输出结果
 
-type T struct {
-	int
-}
+//s1 is : {{1}}
+//如类型 S 包含匿名字段 T，则 S 和 *S 方法集包含 T 方法。
+//s2 is : &{{1}}
+//如类型 S 包含匿名字段 T，则 S 和 *S 方法集包含 T 方法。
 
-func (t T) testT() {
-	fmt.Println("如类型 S 包含匿名字段 T，则 S 和 *S 方法集包含 T 方法。")
-}
+//如类型 S 包含匿名字段 *T，则 S 和 *S 方法集包含 T + *T 方法。
 
-func main() {
-	s1 := S{T{1}}
-	s2 := &s1
-	fmt.Printf("s1 is : %v\n", s1)
-	s1.testT()
-	fmt.Printf("s2 is : %v\n", s2)
-	s2.testT()
-}
+//这条规则说的是当我们嵌入一个类型的指针，嵌入类型的接受者为值类型或指针类型的方法将被提升，可以被外部类型的值或者指针调用。
 
+//type S struct {
+//	T
+//}
+//
+//type T struct {
+//	int
+//}
+//
+//func (t T) testT() {
+//	fmt.Println("如类型 S 包含匿名字段 *T，则 S 和 *S 方法集包含 T 方法")
+//}
+//func (t *T) testP() {
+//	fmt.Println("如类型 S 包含匿名字段 *T，则 S 和 *S 方法集包含 *T 方法")
+//}
+//
+//func main() {
+//	s1 := S{T{1}}
+//	s2 := &s1
+//	fmt.Printf("s1 is : %v\n", s1)
+//	s1.testT()
+//	s1.testP()
+//	fmt.Printf("s2 is : %v\n", s2)
+//	s2.testT()
+//	s2.testP()
+//}
 
-
-
-
-
-
+//输出结果
+//s1 is : {{1}}
+//如类型 S 包含匿名字段 *T，则 S 和 *S 方法集包含 T 方法
+//如类型 S 包含匿名字段 *T，则 S 和 *S 方法集包含 *T 方法
+//s2 is : &{{1}}
+//如类型 S 包含匿名字段 *T，则 S 和 *S 方法集包含 T 方法
+//如类型 S 包含匿名字段 *T，则 S 和 *S 方法集包含 *T 方法
 
