3f0a3995d8f69bbe4fbc1d2b1528198a9da3dc81
wolf <wolf27w@gmail.com>
2021-05-28 10:52:34 +0800

    golang方法

--- base/learn-test.go
+++ base/learn-test.go
@@ -11,3 +11,66 @@ package main
 //一个方法就是一个包含了接受者的函数，接受者可以是命名类型或者结构体类型的一个值或者是一个指针。
 //
 //所有给定类型的方法属于该类型的方法集。
+
+
+//########方法定义
+
+//func (recevier type) methodName(参数列表)(返回值列表){}
+//
+//参数和返回值可以省略
+
+//package main
+//
+//type Test struct{}
+//
+//// 无参数、无返回值
+//func (t Test) method0() {
+//
+//}
+//
+//// 单参数、无返回值
+//func (t Test) method1(i int) {
+//
+//}
+//
+//// 多参数、无返回值
+//func (t Test) method2(x, y int) {
+//
+//}
+//
+//// 无参数、单返回值
+//func (t Test) method3() (i int) {
+//    return
+//}
+//
+//// 多参数、多返回值
+//func (t Test) method4(x, y int) (z int, err error) {
+//    return
+//}
+//
+//// 无参数、无返回值
+//func (t *Test) method5() {
+//
+//}
+//
+//// 单参数、无返回值
+//func (t *Test) method6(i int) {
+//
+//}
+//
+//// 多参数、无返回值
+//func (t *Test) method7(x, y int) {
+//
+//}
+//
+//// 无参数、单返回值
+//func (t *Test) method8() (i int) {
+//    return
+//}
+//
+//// 多参数、多返回值
+//func (t *Test) method9(x, y int) (z int, err error) {
+//    return
+//}
+//
+//func main() {}
