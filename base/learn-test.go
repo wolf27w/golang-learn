a4cf4741f6209708037044271c8ec720db67af4a
wolf <wolf27w@gmail.com>
2021-05-28 11:10:53 +0800

    golang方法

--- base/learn-test.go
+++ base/learn-test.go
@@ -278,7 +278,35 @@ package main
 
 //通过匿名字段，可获得和继承类似的复用能力。依据编译器查找次序，只需在外层定义同名方法，就可以实现 "override"。
 
+//type User struct {
+//	id   int
+//	name string
+//}
+//
+//type Manager struct {
+//	User
+//	title string
+//}
+//
+//func (self *User) ToString() string {
+//	return fmt.Sprintf("User: %p, %v", self, self)
+//}
+//
+//func (self *Manager) ToString() string {
+//	return fmt.Sprintf("Manager: %p, %v", self, self)
+//}
+//
+//func main() {
+//	m := Manager{User{1, "Tom"}, "Administrator"}
+//
+//	fmt.Println(m.ToString())
+//
+//	fmt.Println(m.User.ToString())
+//}
+//输出结果
 
+//Manager: 0xc000090180, &{{1 Tom} Administrator}
+//User: 0xc000090180, &{1 Tom}
 
 
 
