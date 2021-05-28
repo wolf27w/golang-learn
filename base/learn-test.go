1985dcdaaaace58adcc40e336178a24fa63f406c
wolf <wolf27w@gmail.com>
2021-05-28 11:02:47 +0800

    golang方法

--- base/learn-test.go
+++ base/learn-test.go
@@ -145,29 +145,29 @@ import "fmt"
 
 //方法不过是一种特殊的函数，只需将其还原，就知道 receiver T 和 *T 的差别。
 
-type Data struct {
-	x int
-}
-
-func (self Data) ValueTest() { // func ValueTest(self Data);
-	fmt.Printf("Value: %p\n", &self)
-}
-
-func (self *Data) PointerTest() { // func PointerTest(self *Data);
-	fmt.Printf("Pointer: %p\n", self)
-}
-
-func main() {
-	d := Data{}
-	p := &d
-	fmt.Printf("Data: %p\n", p)
-
-	d.ValueTest()   // ValueTest(d)
-	d.PointerTest() // PointerTest(&d)
-
-	p.ValueTest()   // ValueTest(*p)
-	p.PointerTest() // PointerTest(p)
-}
+//type Data struct {
+//	x int
+//}
+//
+//func (self Data) ValueTest() { // func ValueTest(self Data);
+//	fmt.Printf("Value: %p\n", &self)
+//}
+//
+//func (self *Data) PointerTest() { // func PointerTest(self *Data);
+//	fmt.Printf("Pointer: %p\n", self)
+//}
+//
+//func main() {
+//	d := Data{}
+//	p := &d
+//	fmt.Printf("Data: %p\n", p)
+//
+//	d.ValueTest()   // ValueTest(d)
+//	d.PointerTest() // PointerTest(&d)
+//
+//	p.ValueTest()   // ValueTest(*p)
+//	p.PointerTest() // PointerTest(p)
+//}
 //输出结果
 //Data: 0xc0000a8008
 //Value: 0xc0000a8018
@@ -176,14 +176,80 @@ func main() {
 //Pointer: 0xc0000a8008
 
 
+//#########################普通函数与方法的区别
+
+//1.对于普通函数，接收者为值类型时，不能将指针类型的数据直接传递，反之亦然。
+//
+//2.对于方法（如struct的方法），接收者为值类型时，可以直接用指针类型的变量调用方法，反过来同样也可以。
 
 
+//1.普通函数
+//接收值类型参数的函数
+func valueIntTest(a int) int {
+	return a + 10
+}
 
+//接收指针类型参数的函数
+func pointerIntTest(a *int) int {
+	return *a + 10
+}
 
+func structTestValue() {
+	a := 2
+	fmt.Println("valueIntTest:", valueIntTest(a))
+	//函数的参数为值类型，则不能直接将指针作为参数传递
+	//fmt.Println("valueIntTest:", valueIntTest(&a))
+	//compile error: cannot use &a (type *int) as type int in function argument
 
+	b := 5
+	fmt.Println("pointerIntTest:", pointerIntTest(&b))
+	//同样，当函数的参数为指针类型时，也不能直接将值类型作为参数传递
+	//fmt.Println("pointerIntTest:", pointerIntTest(b))
+	//compile error:cannot use b (type int) as type *int in function argument
+}
 
+//2.方法
+type PersonD struct {
+	id   int
+	name string
+}
 
+//接收者为值类型
+func (p PersonD) valueShowName() {
+	fmt.Println(p.name)
+}
 
+//接收者为指针类型
+func (p *PersonD) pointShowName() {
+	fmt.Println(p.name)
+}
+
+func structTestFunc() {
+	//值类型调用方法
+	personValue := PersonD{101, "hello world"}
+	personValue.valueShowName()
+	personValue.pointShowName()
+
+	//指针类型调用方法
+	personPointer := &PersonD{102, "hello golang"}
+	personPointer.valueShowName()
+	personPointer.pointShowName()
+
+	//与普通函数不同，接收者为指针类型和值类型的方法，指针类型和值类型的变量均可相互调用
+}
+
+func main() {
+	structTestValue()
+	structTestFunc()
+}
+
+//输出结果
+//valueIntTest: 12
+//pointerIntTest: 15
+//hello world
+//hello world
+//hello golang
+//hello golang
 
 
 
