60d4a2a8a3a96840ffc3edb53a485b203bbad753
wolf <wolf27w@gmail.com>
2021-05-28 10:45:00 +0800

    golang函数

--- base/learn-func.go
+++ base/learn-func.go
@@ -1910,7 +1910,30 @@ package main
 //函数中通过调用testing.T的Error, Errorf, FailNow, Fatal, FatalIf方法，说明测试不通过，调用Log方法用来记录测试的信息。
 
 
+package gotest
 
+import (
+"testing"
+)
+
+//func Test_Division_1(t *testing.T) {
+//	if i, e := Division(6, 2); i != 3 || e != nil { //try a unit test on function
+//		t.Error("除法函数测试没通过") // 如果不是如预期的那么就报错
+//	} else {
+//		t.Log("第一个测试通过了") //记录一些你期望记录的信息
+//	}
+//}
+//
+//func Test_Division_2(t *testing.T) {
+//	t.Error("就是不通过")
+//}
+//执行结果
+
+//--- FAIL: Test_Division_2 (0.00 seconds)
+//gotest_test.go:16: 就是不通过
+//FAIL
+//exit status 1
+//FAIL    gotest    0.013s
 
 
 
