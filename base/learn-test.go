fa8436b54fb63450df3461718ffb3a4f53ef3bbd
wolf <wolf27w@gmail.com>
2021-05-28 10:54:26 +0800

    golang方法

--- base/learn-test.go
+++ base/learn-test.go
@@ -76,9 +76,36 @@ package main
 //func main() {}
 
 
+//下面定义一个结构体类型和该类型的一个方法：
 
 
 
+
+
+//结构体
+//type User struct {
+//	Name  string
+//	Email string
+//}
+//
+////方法
+//func (u User) Notify() {
+//	fmt.Printf("%v : %v \n", u.Name, u.Email)
+//}
+//func main() {
+//	// 值类型调用方法
+//	u1 := User{"golang", "golang@golang.com"}
+//	u1.Notify()
+//	// 指针类型调用方法
+//	u2 := User{"go", "go@go.com"}
+//	u3 := &u2
+//	u3.Notify()
+//}
+
+//输出结果
+
+//golang : golang@golang.com
+//go : go@go.com
 
 
 
