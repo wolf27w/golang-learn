0acca0d100664e3268f6e8b85e12dd066878285f
wolf <wolf27w@gmail.com>
2021-05-28 10:57:42 +0800

    golang方法

--- base/learn-test.go
+++ base/learn-test.go
@@ -110,10 +110,36 @@ package main
 //注释：首先我们定义了一个叫做 User 的结构体类型，然后定义了一个该类型的方法叫做 Notify，该方法的接受者是一个 User 类型的值。要调用 Notify 方法我们需要一个 User 类型的值或者指针。
 
 
+//Go 调整和解引用指针使得调用可以被执行。注意，当接受者不是一个指针时，该方法操作对应接受者的值的副本(意思就是即使你使用了指针调用函数，但是函数的接受者是值类型，所以函数内部操作还是对副本的操作，而不是指针操作。
+
+//我们修改 Notify 方法，让它的接受者使用指针类型：
+
+//结构体
+//type User struct {
+//	Name  string
+//	Email string
+//}
+//
+////方法
+//func (u *User) Notify() {
+//	fmt.Printf("%v : %v \n", u.Name, u.Email)
+//}
+//func main() {
+//	// 值类型调用方法
+//	u1 := User{"golang", "golang@golang.com"}
+//	u1.Notify()
+//	// 指针类型调用方法
+//	u2 := User{"go", "go@go.com"}
+//	u3 := &u2
+//	u3.Notify()
+//}
+
+//输出结果
+//golang : golang@golang.com
+//go : go@go.com
 
 
-
-
+//注意：当接受者是指针时，即使用值类型调用那么函数内部也是对指针的操作。
 
 
 
