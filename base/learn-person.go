9dfed0ffcc33afc4e7a588787f8b4bd8d511f919
wolf <wolf27w@gmail.com>
2021-05-28 13:31:34 +0800

    golang面向对象

--- base/learn-person.go
+++ base/learn-person.go
@@ -125,9 +125,24 @@ package main
 
 //########################接口
 
+//接口（interface）定义了一个对象的行为规范，只定义规范不实现，由具体的对象来实现规范的细节。
 
+//interface是一组method的集合，是duck-type programming的一种体现。接口做的事情就像是定义一个协议（规则），只要一台机器有洗衣服和甩干的功能，我就称它为洗衣机。不关心属性（数据），只关心行为（方法）
 
-
+//type Cat struct{}
+//
+//func (c Cat) Say() string { return "喵喵喵" }
+//
+//type Dog struct{}
+//
+//func (d Dog) Say() string { return "汪汪汪" }
+//
+//func main() {
+//    c := Cat{}
+//    fmt.Println("猫:", c.Say())
+//    d := Dog{}
+//    fmt.Println("狗:", d.Say())
+//}
 
 
 
