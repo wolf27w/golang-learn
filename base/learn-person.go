5e0bb8af9c215eb1aa2b8fd3490f02a077629b00
wolf <wolf27w@gmail.com>
2021-05-31 09:57:41 +0800

    golang面向对象

--- base/learn-person.go
+++ base/learn-person.go
@@ -335,7 +335,32 @@ package main
 //    move()
 //}
 
+//dog既可以实现Sayer接口，也可以实现Mover接口。
 
+//type dog struct {
+//    name string
+//}
+//
+//// 实现Sayer接口
+//func (d dog) say() {
+//    fmt.Printf("%s会叫汪汪汪\n", d.name)
+//}
+//
+//// 实现Mover接口
+//func (d dog) move() {
+//    fmt.Printf("%s会动\n", d.name)
+//}
+//
+//func main() {
+//    var x Sayer
+//    var y Mover
+//
+//    var a = dog{name: "旺财"}
+//    x = a
+//    y = a
+//    x.say()
+//    y.move()
+//}
 
 
 
