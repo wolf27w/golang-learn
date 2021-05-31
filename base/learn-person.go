830f750686ed48acf358e70d4caa12dbb6167259
wolf <wolf27w@gmail.com>
2021-05-31 10:25:57 +0800

    golang面向对象

--- base/learn-person.go
+++ base/learn-person.go
@@ -410,6 +410,32 @@ package main
 //    旺财会跑
 //    保时捷速度70迈
 
+//并且一个接口的方法，不一定需要由一个类型完全实现，接口的方法可以通过在类型中嵌入其他类型或者结构体来实现。
+
+//// WashingMachine 洗衣机
+//type WashingMachine interface {
+//    wash()
+//    dry()
+//}
+//
+//// 甩干器
+//type dryer struct{}
+//
+//// 实现WashingMachine接口的dry()方法
+//func (d dryer) dry() {
+//    fmt.Println("甩一甩")
+//}
+//
+//// 海尔洗衣机
+//type haier struct {
+//    dryer //嵌入甩干器
+//}
+//
+//// 实现WashingMachine接口的wash()方法
+//func (h haier) wash() {
+//    fmt.Println("洗刷刷")
+//}
+
 
 
 
