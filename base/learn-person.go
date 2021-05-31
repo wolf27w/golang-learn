6225ff25672af6b40fa7897828c5fabc8a9a4f5c
wolf <wolf27w@gmail.com>
2021-05-31 10:29:31 +0800

    golang面向对象

--- base/learn-person.go
+++ base/learn-person.go
@@ -501,6 +501,26 @@ package main
 
 //####################空接口的应用
 
+//空接口作为函数的参数
+
+//使用空接口实现可以接收任意类型的函数参数。
+
+//// 空接口作为函数参数
+//func show(a interface{}) {
+//    fmt.Printf("type:%T value:%v\n", a, a)
+//}
+
+//###################空接口作为map的值
+
+//使用空接口实现可以保存任意值的字典。
+
+
+//// 空接口作为map值
+//    var studentInfo = make(map[string]interface{})
+//    studentInfo["name"] = "李白"
+//    studentInfo["age"] = 18
+//    studentInfo["married"] = false
+//    fmt.Println(studentInfo)
 
 
 
