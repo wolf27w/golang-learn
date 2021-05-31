ac658c100658ecf5deeb0ef53984589a76750f30
wolf <wolf27w@gmail.com>
2021-05-31 10:32:46 +0800

    golang面向对象

--- base/learn-person.go
+++ base/learn-person.go
@@ -546,3 +546,20 @@ package main
 //}
 
 //上面的示例中如果要断言多次就需要写多个if判断，这个时候我们可以使用switch语句来实现：
+
+//func justifyType(x interface{}) {
+//	switch v := x.(type) {
+//	case string:
+//		fmt.Printf("x is a string，value is %v\n", v)
+//	case int:
+//		fmt.Printf("x is a int is %v\n", v)
+//	case bool:
+//		fmt.Printf("x is a bool is %v\n", v)
+//	default:
+//		fmt.Println("unsupport type！")
+//	}
+//}
+
+//因为空接口可以存储任意类型值的特点，所以空接口在Go语言中的使用十分广泛。
+//
+//关于接口需要注意的是，只有当有两个或两个以上的具体类型必须以相同的方式进行处理时才需要定义接口。不要为了接口而写接口，那样只会增加不必要的抽象，导致不必要的运行时损耗。
