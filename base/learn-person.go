0daae78e5da30f068a2167255051ab2416a060e8
wolf <wolf27w@gmail.com>
2021-05-31 10:26:44 +0800

    golang面向对象

--- base/learn-person.go
+++ base/learn-person.go
@@ -436,7 +436,27 @@ package main
 //    fmt.Println("洗刷刷")
 //}
 
+//####################接口嵌套
 
+//接口与接口间可以通过嵌套创造出新的接口。
+
+//// Sayer 接口
+//type Sayer interface {
+//    say()
+//}
+//
+//// Mover 接口
+//type Mover interface {
+//    move()
+//}
+//
+//// 接口嵌套
+//type animal interface {
+//    Sayer
+//    Mover
+//}
+
+//嵌套得到的接口的使用与普通接口一样，这里我们让cat实现animal接口：
 
 
 
