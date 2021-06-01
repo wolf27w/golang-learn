abcf8c571418c8fccea390150b6ac74ae825ba16
wolf <wolf27w@gmail.com>
2021-06-01 10:24:52 +0800

    golang网络编程

--- base/learn-socket.go
+++ base/learn-socket.go
@@ -1,4 +1,5 @@
 package main
+
 //
 //import (
 //	"fmt"
@@ -85,8 +86,103 @@ package main
 
 //TCP就像货到付款的快递，送到家还必须见到你人才算一整套流程。UDP就像某快递快递柜一扔就走管你收到收不到，一般直播用UDP
 
+//##########################TCP编程########################
+
+//TCP协议，TCP/IP(Transmission Control Protocol/Internet Protocol) 即传输控制协议/网间协议，是一种面向连接（连接导向）的、可靠的、基于字节流的传输层（Transport layer）通信协议，因为是面向连接的协议，数据像水流一样传输，会存在黏包问题。
+
+//TCP服务端，一个TCP服务端可以同时连接很多个客户端，例如世界各地的用户使用自己电脑上的浏览器访问淘宝网。因为Go语言中创建多个goroutine实现并发非常方便和高效，所以我们可以每建立一次链接就创建一个goroutine去处理。
+
+//TCP处理流程：
+//1.监听端口
+//    2.接收客户端请求建立链接
+//    3.创建goroutine处理链接。
+
+//TCP客户端
+
+// 1.建立与服务端的链接
+//    2.进行数据收发
+//    3.关闭链接
 
 
+// tcp/server/main.go
+
+// TCP server端
+
+// 处理函数
+//func process(conn net.Conn) {
+//	defer conn.Close() // 关闭连接
+//	for {
+//		reader := bufio.NewReader(conn)
+//		var buf [128]byte
+//		n, err := reader.Read(buf[:]) // 读取数据
+//		if err != nil {
+//			fmt.Println("read from client failed, err:", err)
+//			break
+//		}
+//		recvStr := string(buf[:n])
+//		fmt.Println("收到client端发来的数据：", recvStr)
+//		conn.Write([]byte(recvStr)) // 发送数据
+//	}
+//}
+//
+//func main() {
+//	listen, err := net.Listen("tcp", "127.0.0.1:20000")
+//	if err != nil {
+//		fmt.Println("listen failed, err:", err)
+//		return
+//	}
+//	for {
+//		conn, err := listen.Accept() // 建立连接
+//		if err != nil {
+//			fmt.Println("accept failed, err:", err)
+//			continue
+//		}
+//		go process(conn) // 启动一个goroutine处理连接
+//	}
+//}
+
+//TCP客户端
+
+//package main
+//
+//import (
+//"bufio"
+//"fmt"
+//"net"
+//"os"
+//"strings"
+//)
+//
+//// tcp/client/main.go
+//
+//// 客户端
+//func main() {
+//	conn, err := net.Dial("tcp", "127.0.0.1:20000")
+//	if err != nil {
+//		fmt.Println("err :", err)
+//		return
+//	}
+//	defer conn.Close() // 关闭连接
+//	inputReader := bufio.NewReader(os.Stdin)
+//	for {
+//		input, _ := inputReader.ReadString('\n') // 读取用户输入
+//		inputInfo := strings.Trim(input, "\r\n")
+//		if strings.ToUpper(inputInfo) == "Q" { // 如果输入q就退出
+//			return
+//		}
+//		_, err = conn.Write([]byte(inputInfo)) // 发送数据
+//		if err != nil {
+//			return
+//		}
+//		//buf := [512]byte{}
+//		//n, err := conn.Read(buf[:])
+//		//if err != nil {
+//		//	fmt.Println("recv failed, err:", err)
+//		//	return
+//		//}
+//		//fmt.Println(string(buf[:n]))
+//	}
+//}
 
 
 
