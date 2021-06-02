8377eb4ac0cd52a693e3ed6c2abaf3f95da4a3a0
wolf <wolf27w@gmail.com>
2021-06-02 09:49:20 +0800

    golang网络编程

--- base/learn-socket.go
+++ base/learn-socket.go
@@ -519,6 +519,80 @@ package main
 
 //运行      go run server.go hub.go data.go connection.go
 
+//运行之后执行local.html文件
+
+//server.go
+
+//package main
+//
+//import (
+//    "fmt"
+//    "net/http"
+//
+//    "github.com/gorilla/mux"
+//)
+//
+//func main() {
+//    router := mux.NewRouter()
+//    go h.run()
+//    router.HandleFunc("/ws", myws)
+//    if err := http.ListenAndServe("127.0.0.1:8080", router); err != nil {
+//        fmt.Println("err:", err)
+//    }
+//}
+
+
+//hub.go
+
+//package main
+//
+//import "encoding/json"
+//
+//var h = hub{
+//    c: make(map[*connection]bool),
+//    u: make(chan *connection),
+//    b: make(chan []byte),
+//    r: make(chan *connection),
+//}
+//
+//type hub struct {
+//    c map[*connection]bool
+//    b chan []byte
+//    r chan *connection
+//    u chan *connection
+//}
+//
+//func (h *hub) run() {
+//    for {
+//        select {
+//        case c := <-h.r:
+//            h.c[c] = true
+//            c.data.Ip = c.ws.RemoteAddr().String()
+//            c.data.Type = "handshake"
+//            c.data.UserList = user_list
+//            data_b, _ := json.Marshal(c.data)
+//            c.sc <- data_b
+//        case c := <-h.u:
+//            if _, ok := h.c[c]; ok {
+//                delete(h.c, c)
+//                close(c.sc)
+//            }
+//        case data := <-h.b:
+//            for c := range h.c {
+//                select {
+//                case c.sc <- data:
+//                default:
+//                    delete(h.c, c)
+//                    close(c.sc)
+//                }
+//            }
+//        }
+//    }
+//}
+
+//data.go
+
+//
 
 
 
