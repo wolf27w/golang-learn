759d8f8e4cd4c1ff5a061cdee75f48b4a8854060
wolf <wolf27w@gmail.com>
2021-09-29 10:40:44 +0800

    golang并发编程

--- base/learn-concurrency.go
+++ base/learn-concurrency.go
@@ -221,7 +221,9 @@ package main
 //runtime.Goexit结束当前goroutine,其他的goroutine不受影响,主程序也一样继续运行
 //os.Exit会结束当前程序,不管你三七二十一
 
+//####runtime.GOMAXPROCS
 
+//Go运行时的调度器使用GOMAXPROCS参数来确定需要使用多少个OS线程来同时执行Go代码。默认值是机器上的CPU核心数
 
 
 
