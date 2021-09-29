9befa62defea2dbfc0d0598ca7a6f57e820f852b
wolf <wolf27w@gmail.com>
2021-09-29 10:41:57 +0800

    golang并发编程

--- base/learn-concurrency.go
+++ base/learn-concurrency.go
@@ -225,7 +225,7 @@ package main
 
 //Go运行时的调度器使用GOMAXPROCS参数来确定需要使用多少个OS线程来同时执行Go代码。默认值是机器上的CPU核心数，例如在一个8核心的机器上，调度器会把Go代码同时调度到8个OS线程上（GOMAXPROCS是m:n调度中的n）
 
-
+//Go语言中可以通过runtime.GOMAXPROCS()函数设置当前程序并发时占用的CPU逻辑核心数。
 
 
 
