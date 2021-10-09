2e9bd0f2c7e868b6292222425775d94220f44a7a
wolf <wolf27w@gmail.com>
2021-10-09 14:16:16 +0800

    golang并发编程

--- base/learn-concurrency.go
+++ base/learn-concurrency.go
@@ -317,7 +317,13 @@ package main
 
 // ### Channel
 
-//channel
+//channel，其实单纯的对将函数并发执行是没有任何意义对，函数与函数之间需要交换数据才能提醒并发函数对意义，虽然可以使用共享内存进行数据交换，但是共享内存在不同对goroutine中容易发生竟态问题，为了保证数据交换对正确性，必须使用互斥量对内存进行加锁，这种做法势必造成性能问题
+
+//Go语言的并发模型是CSP（Communicating Sequential Processes），提倡通过通信共享内存而不是通过共享内存而实现通信
+
+//如果说goroutine是Go程序并发的执行体，channel就是它们之间的连接。channel是可以让一个goroutine发送特定值到另一个goroutine的通信机制
+
+//Go 语言中的通道（channel）是一种特殊的类型。通道像一个传送带或者队列，总是遵循先入先出（First In First Out）的规则，保证收发数据的顺序。每一个通道都是一个具体类型的导管，也就是声明channel的时候需要为其指定元素类型。
 
 
 
