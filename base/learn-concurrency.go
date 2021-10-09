4f5152e0de3519fe1098feba9200bee5adaa658b
wolf <wolf27w@gmail.com>
2021-10-09 15:53:28 +0800

    golang并发编程

--- base/learn-concurrency.go
+++ base/learn-concurrency.go
@@ -511,11 +511,54 @@ package main
 
 //从上面的例子中我们看到有两种方式在接收值的时候判断通道是否被关闭，我们通常使用的是for range的方式。
 
+//  单向通道
+
+//有的时候我们会将通道作为参数在多个任务函数间传递，很多时候我们在不同的任务函数中使用通道都会对其进行限制，比如限制通道在函数中只能发送或只能接收
+
+//Go语言中提供了单向通道来处理这种情况。例如，我们把上面的例子改造如下：
+
+//func counter(out chan<- int)  {
+//	for i := 0; i < 10; i++ {
+//		out <- i
+//	}
+//	close(out)
+//}
+//func squarer(out chan<- int, in <- chan  int)  {
+//	for i := range in {
+//		out <- i * i
+//	}
+//	close(out)
+//}
+//func printer(in <-chan int)  {
+//	for i := range in {
+//		fmt.Println(i)
+//	}
+//}
+//
+//func main()  {
+//	ch1 := make(chan int)
+//	ch2 := make(chan int)
+//	go counter(ch1)
+//	go squarer(ch2,ch1)
+//	printer(ch2)
+//}
+
+//其中
+//    1.chan<- int是一个只能发送的通道，可以发送但是不能接收；
+//    2.<-chan int是一个只能接收的通道，可以接收但是不能发送。
 
 
-
-
-
+//输出结果
+//0
+//1
+//4
+//9
+//16
+//25
+//36
+//49
+//64
+//81
 
 
 
