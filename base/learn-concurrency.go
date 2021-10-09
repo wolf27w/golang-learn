385f87b1a423b1688bc2452b9402b670a4e77a19
wolf <wolf27w@gmail.com>
2021-10-09 14:18:03 +0800

    golang并发编程

--- base/learn-concurrency.go
+++ base/learn-concurrency.go
@@ -325,7 +325,14 @@ package main
 
 //Go 语言中的通道（channel）是一种特殊的类型。通道像一个传送带或者队列，总是遵循先入先出（First In First Out）的规则，保证收发数据的顺序。每一个通道都是一个具体类型的导管，也就是声明channel的时候需要为其指定元素类型。
 
+//channel是一种类型，一种引用类型。声明通道类型的格式如下：
 
+//var 变量 chan 元素类型
+
+//举例
+//var ch1 chan int   // 声明一个传递整型的通道
+//var ch2 chan bool  // 声明一个传递布尔型的通道
+//var ch3 chan []int // 声明一个传递int切片的通道
 
 
 
