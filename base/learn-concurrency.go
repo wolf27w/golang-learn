f8e71681a5ce0318012a548029528b403951b22a
wolf <wolf27w@gmail.com>
2021-10-11 10:22:30 +0800

    golang并发编程

--- base/learn-concurrency.go
+++ base/learn-concurrency.go
@@ -700,57 +700,78 @@ import (
 //Time 时间到了，执行只执行一次
 
 
-func main() {
-	// 1.timer基本使用
-	//timer1 := time.NewTimer(2 * time.Second)
-	//t1 := time.Now()
-	//fmt.Printf("t1:%v\n", t1)
-	//t2 := <-timer1.C
-	//fmt.Printf("t2:%v\n", t2)
+//func main() {
+//	// 1.timer基本使用
+//	//timer1 := time.NewTimer(2 * time.Second)
+//	//t1 := time.Now()
+//	//fmt.Printf("t1:%v\n", t1)
+//	//t2 := <-timer1.C
+//	//fmt.Printf("t2:%v\n", t2)
+//
+//	// 2.验证timer只能响应1次
+//	//timer2 := time.NewTimer(time.Second)
+//	//for {
+//	// <-timer2.C
+//	// fmt.Println("时间到")
+//	//}
+//
+//	// 3.timer实现延时的功能
+//	//(1)
+//	//time.Sleep(time.Second)
+//	//(2)
+//	//timer3 := time.NewTimer(2 * time.Second)
+//	//<-timer3.C
+//	//fmt.Println("2秒到")
+//	//(3)
+//	//<-time.After(2*time.Second)
+//	//fmt.Println("2秒到")
+//
+//	// 4.停止定时器
+//	//timer4 := time.NewTimer(2 * time.Second)
+//	//go func() {
+//	// <-timer4.C
+//	// fmt.Println("定时器执行了")
+//	//}()
+//	//b := timer4.Stop()
+//	//if b {
+//	// fmt.Println("timer4已经关闭")
+//	//}
+//
+//	// 5.重置定时器
+//	timer5 := time.NewTimer(3 * time.Second)
+//	timer5.Reset(1 * time.Second)
+//	fmt.Println(time.Now())
+//	fmt.Println(<-timer5.C)
+//	for {
 
-	// 2.验证timer只能响应1次
-	//timer2 := time.NewTimer(time.Second)
-	//for {
-	// <-timer2.C
-	// fmt.Println("时间到")
-	//}
-
-	// 3.timer实现延时的功能
-	//(1)
-	//time.Sleep(time.Second)
-	//(2)
-	//timer3 := time.NewTimer(2 * time.Second)
-	//<-timer3.C
-	//fmt.Println("2秒到")
-	//(3)
-	//<-time.After(2*time.Second)
-	//fmt.Println("2秒到")
-
-	// 4.停止定时器
-	//timer4 := time.NewTimer(2 * time.Second)
-	//go func() {
-	// <-timer4.C
-	// fmt.Println("定时器执行了")
-	//}()
-	//b := timer4.Stop()
-	//if b {
-	// fmt.Println("timer4已经关闭")
-	//}
-
-	// 5.重置定时器
-	timer5 := time.NewTimer(3 * time.Second)
-	timer5.Reset(1 * time.Second)
-	fmt.Println(time.Now())
-	fmt.Println(<-timer5.C)
-
-}
+//	}
+//}
 //输出结果
 //2021-10-11 10:18:04.712797 +0800 CST m=+0.000111222
 //2021-10-11 10:18:05.716333 +0800 CST m=+1.003695397
 
+func main() {
+	// 1.获取ticker对象
+	ticker := time.NewTicker(1 * time.Second)
+	i := 0
+	// 子协程
+	go func() {
+		for {
+			//<-ticker.C
+			i++
+			fmt.Println(<-ticker.C)
+			if i == 5 {
+				//停止
+				ticker.Stop()
+			}
+		}
+	}()
+	for {
 
+	}
+}
 
-
+//输出结果
 
 
 
