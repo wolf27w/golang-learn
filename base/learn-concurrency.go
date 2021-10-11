cc338c1dbdfb7732362185f49011a07dd7e1b5ff
wolf <wolf27w@gmail.com>
2021-10-11 16:06:14 +0800

    golang并发编程

--- base/learn-concurrency.go
+++ base/learn-concurrency.go
@@ -893,21 +893,159 @@ package main
 
 //有时多个goroutine同时操作一个资源，这种情况就会发生竟态问题(数据竟态).例如：现实生活中的例子有十字路口被各个方向的的汽车竞争；还有火车上的卫生间被车厢里的人竞争。
 
+//var x int64
+//var wg sync.WaitGroup
+//
+//func add()  {
+//	for i := 0; i < 10; i++ {
+//		x = x + 1
+//		fmt.Println("这是x的值",x)
+//	}
+//	wg.Done()
+//}
+//
+//func main()  {
+//	wg.Add(2)
+//	go add()
+//	go add()
+//	wg.Wait()
+//	fmt.Println(x)
+//}
+
+//输出结果
+
+//这是x的值 1
+//这是x的值 3
+//这是x的值 4
+//这是x的值 5
+//这是x的值 6
+//这是x的值 7
+//这是x的值 8
+//这是x的值 9
+//这是x的值 10
+//这是x的值 11
+//这是x的值 2
+//这是x的值 12
+//这是x的值 13
+//这是x的值 14
+//这是x的值 15
+//这是x的值 16
+//这是x的值 17
+//这是x的值 18
+//这是x的值 19
+//这是x的值 20
+//20
+
+//上面的代码开启了两个goroutine去累加遍历x的值，这两个goroutine在访问和修改x遍历的时候就会存在数据竞争，导致最后的结果与期待的不符。
+
+//  互斥锁
+
+//互斥锁是一种常用的控制共享资源访问的方法，她能够保证同时只有一个goroutine可以访问共享资源，go语言中使用sync包的Mutex类型来实现互斥锁，使用互斥锁来修复上面代码的问题：
 
 
+//var x int64
+//var wg sync.WaitGroup
+//var lock sync.Mutex
+//
+//func add()  {
+//	for i := 0; i <10; i++ {
+//		lock.Lock()  //加锁
+//		x = x + 1
+//		fmt.Println("这个是x的值",x)
+//		lock.Unlock() //解锁
+//	}
+//	wg.Done()
+//}
+//
+//func main()  {
+//	wg.Add(2)
+//	go add()
+//	go add()
+//	wg.Wait()
+//	fmt.Println(x)
+//}
+
+//输出结果
+
+//这个是x的值 1
+//这个是x的值 2
+//这个是x的值 3
+//这个是x的值 4
+//这个是x的值 5
+//这个是x的值 6
+//这个是x的值 7
+//这个是x的值 8
+//这个是x的值 9
+//这个是x的值 10
+//这个是x的值 11
+//这个是x的值 12
+//这个是x的值 13
+//这个是x的值 14
+//这个是x的值 15
+//这个是x的值 16
+//这个是x的值 17
+//这个是x的值 18
+//这个是x的值 19
+//这个是x的值 20
+//20
 
 
+// 使用互斥锁能够保证同一时间有且只有一个goroutine进入临界区，其他的goroutine则在等待锁，当互斥锁释放后，等待的goroutine才可以获取锁进入临界区，多个goroutine同时等待一个锁时，唤醒的策略时随机的。
+
+//   读写互斥锁
+
+//互斥锁是完全的互斥，但是有些环境是读多写少的，当我们并发的去读取资源不涉及资源修改的时候是没有必要加锁的，这种场景中使用读写锁是更好的一种选择，读写锁在Go语言中使用sync包中的RWMutex类型
+
+//读写锁分为两种，读锁和写锁，当一个goroutine获取读锁之后，其他的goroutine如果是获取读锁会继续获得锁，如果是获取写锁就会等待，当一个goroutine获取写锁之后，其他的goroutine无论是获取读锁还是写锁都会等待
+
+//读写锁实例
+
+//var (
+//	x    int64
+//	wg   sync.WaitGroup
+//	lock sync.Mutex
+//	rwlock sync.RWMutex
+//)
+//
+//func write()  {
+//	// lock.Lock()   //加互斥锁
+//	rwlock.Lock()    //加写锁
+//	x = x + 1
+//	time.Sleep(10 * time.Millisecond)  //假设读操作耗时10毫秒
+//	rwlock.Unlock()     //解写锁
+//	wg.Done()     //解互斥锁
+//}
+//
+//func read()  {
+//	//local.Lock()   //加互斥锁
+//	rwlock.RLock()     //加读锁
+//	time.Sleep(time.Millisecond)   //假设读操作耗时1毫秒
+//	rwlock.RUnlock()      //解读锁
+//	wg.Done()    //解互斥锁
+//}
+//
+//func main()  {
+//	start := time.Now()
+//	for i := 0; i < 10; i ++ {
+//		wg.Add(1)
+//		go write()
+//	}
+//	for i := 0; i < 1000; i++ {
+//		wg.Add(1)
+//		go read()
+//	}
+//	wg.Wait()
+//	end := time.Now()
+//	fmt.Println(end.Sub(start))
+//}
+
+//输出结果
+//112.646527ms
+
+//需要注意的是读写锁非常适合读多写少的场景，如果读和写的操作差别不大，读写锁读又是就发挥不出来了。
 
 
-
-
-
-
-
-
-
-
-
+//
 
 
 
