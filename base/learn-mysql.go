5629659ade8a3247a43bdbbcf4d1136b6e8a86e5
wolf <wolf27w@gmail.com>
2021-10-13 14:01:08 +0800

    mysql

--- base/learn-mysql.go
+++ base/learn-mysql.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+
+)
+
+func init()  {
+	database, err := sqlx.Open("mysql","root@tcp(127.0.0.1:3306)/test")
+	if err != nil {
+		fmt.Println("open mysql failed",err)
+		return
+	}
+	Db = database
+	defer  db.Close()
+}
+
+func main()  {
+	r, err := Db.Exec("select * from palce")
+	if err != nil {
+		fmt.Println("select failed",err)
+		return
+	}
+	fmt.Println("seect succ",r)
+}
