7e61e208ed8984739c7403cb8fb52e647992c62b
wolf <wolf27w@gmail.com>
2021-10-13 14:43:00 +0800

    mysql

--- base/learn-mysql.go
+++ base/learn-mysql.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
@@ -22,26 +23,25 @@ type Place struct {
 var Db *sqlx.DB
 
 func init() {
+
 	database, err := sqlx.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test")
 	if err != nil {
 		fmt.Println("open mysql failed,", err)
 		return
 	}
+
 	Db = database
 	//defer db.Close()  // 注意这行代码要写在上面err判断的下面
 }
 
 func main() {
-	r, err := Db.Exec("insert into person(username, sex, email)values(?, ?, ?)", "stu001", "man", "stu01@qq.com")
-	if err != nil {
-		fmt.Println("exec failed, ", err)
-		return
-	}
-	id, err := r.LastInsertId()
+
+	var person []Person
+	err := Db.Select(&person, "select * from person ")
 	if err != nil {
 		fmt.Println("exec failed, ", err)
 		return
 	}
 
-	fmt.Println("insert succ:", id)
+	fmt.Println("select succ:", person)
 }
