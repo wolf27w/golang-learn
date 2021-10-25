a24bb2f367303e60dd0013ce92a976ac792d55ec
wolf <wolf27w@gmail.com>
2021-10-25 10:32:14 +0800

    redis

--- base/learn-redis.go
+++ base/learn-redis.go
@@ -221,3 +221,36 @@
 //    "abc"
 //    127.0.0.1:6379> lpop book_list
 //    (nil)
+
+
+//    #####Hash表
+
+package main
+
+import (
+    "fmt"
+    "github.com/gomodule/redigo/redis"
+)
+
+func main() {
+    c, err := redis.Dial("tcp", "10.123.6.236:6379")
+    if err != nil {
+        fmt.Println("conn redis failed,", err)
+        return
+    }
+
+    defer c.Close()
+    _, err = c.Do("HSet", "books", "abc", 100)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    r, err := redis.Int(c.Do("HGet", "books", "abc"))
+    if err != nil {
+        fmt.Println("get abc failed,", err)
+        return
+    }
+
+    fmt.Println(r)
+}
