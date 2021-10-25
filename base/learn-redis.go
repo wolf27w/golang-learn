bb9498265940fee5eecf2cf0916034d9477adf95
wolf <wolf27w@gmail.com>
2021-10-25 10:22:53 +0800

    redis

--- base/learn-redis.go
+++ base/learn-redis.go
@@ -146,3 +146,33 @@
 
 //   ####设置过期时间
 
+package main
+
+import (
+    "fmt"
+    "github.com/gomodule/redigo/redis"
+)
+
+func main() {
+    c, err := redis.Dial("tcp", "10.123.6.236:6379")
+    if err != nil {
+        fmt.Println("conn redis failed,", err)
+        return
+    }
+
+    defer c.Close()
+    _, err = c.Do("expire", "abc", 10)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+}
+
+//  Redis命令行窗口：
+
+//   127.0.0.1:6379> get abc
+//    "100"
+//
+//    # 10秒后过期
+//    127.0.0.1:6379> get abc
+//    (nil)
