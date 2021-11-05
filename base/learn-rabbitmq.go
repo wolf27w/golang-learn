623b9ae7cf87315bb2165f37db5556d81a235bb7
wolf <wolf27w@gmail.com>
2021-11-05 11:26:51 +0800

    rabbitmq

--- base/learn-rabbitmq.go
+++ base/learn-rabbitmq.go
@@ -1104,6 +1104,30 @@
 
 //mainpublish.go代码
 
+//package main
+//
+//import (
+//    "fmt"
+//    "strconv"
+//    "time"
+//
+//    "github.com/student/kuteng-RabbitMQ/RabbitMQ"
+//)
+//
+//func main() {
+//    kutengOne := RabbitMQ.NewRabbitMQTopic("exKutengTopic", "kuteng.topic.one")
+//    kutengTwo := RabbitMQ.NewRabbitMQTopic("exKutengTopic", "kuteng.topic.two")
+//    for i := 0; i <= 100; i++ {
+//        kutengOne.PublishTopic("Hello kuteng topic one!" + strconv.Itoa(i))
+//        kutengTwo.PublishTopic("Hello kuteng topic Two!" + strconv.Itoa(i))
+//        time.Sleep(1 * time.Second)
+//        fmt.Println(i)
+//    }
+//
+//}
+
+
+
 
 
 
