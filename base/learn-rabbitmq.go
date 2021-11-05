022d5df0bf51f54f679cc4c14fc207e1bb35bf93
wolf <wolf27w@gmail.com>
2021-11-05 11:26:13 +0800

    rabbitmq

--- base/learn-rabbitmq.go
+++ base/learn-rabbitmq.go
@@ -920,3 +920,41 @@
 //	kutengtwo := RabbitMQ.NewRabbitMQRouting("kuteng", "kuteng_two")
 //	kutengtwo.RecieveRouting()
 //}
+
+//   Topic模式（话题模式，一个消息被多个消费者获取，消息的目标queue可用BindingKey以通配符，（#：一个或多个词，*：一个词）的方式指定
+
+//星号井号代表通配符
+//星号代表多个单词,井号代表一个单词
+//路由功能添加模糊匹配
+//消息产生者产生消息,把消息交给交换机
+//交换机根据key的规则模糊匹配到对应的队列,由队列的监听消费者接收消息消费
+
+//    目录结构
+
+//kuteng-RabbitMQ
+//
+//-RabbitMQ
+//
+//--rabitmq.go //这个是RabbitMQ的封装
+//
+//-publish
+//
+//--mainpublish.go //Publish 先启动
+//
+//-recieve1
+//
+//--mainrecieve.go
+//
+//-recieve2
+//
+//--mainrecieve.go
+//
+//rabitmq.go代码
+
+
+
+
+
+
+
+
