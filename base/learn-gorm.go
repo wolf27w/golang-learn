163c187f6abb4721aab4bc1d22ad18ab4c3b9dc4
wolf <wolf27w@gmail.com>
2021-11-11 15:19:57 +0800

    gorm

--- base/learn-gorm.go
+++ base/learn-gorm.go
@@ -1620,6 +1620,28 @@
 //
 //db.SetLogger(log.New(os.Stdout, "\r\n", 0))
 
+//1. 更新日志
+//1.1. v2.0
+//WIP
+//
+//1.2. v1.0 - 2016.04.27
+//破坏性变更
+//
+//gorm.Open 返回类型为 *gorm.DB 而不是 gorm.DB ；
+//更新只会更新更改的字段
+//只会使用 deleted_at IS NULL 来检测软删除
+//新的 ToDBName 逻辑
+//在 GORM 将 struct，Field 的名称转换为 db 名称之前，只有那些来自 golint 的常见初始化（如HTTP，URI）是特殊处理的，所以 HTTP 的数据库名称是 http，而不是 h_t_t_p。
+//
+//但是像一些不在列表里的缩写，如 SKU db 名称为 s_k_u，这次升级修复了此问题。
+//
+//错误 RecordNotFound 重命名为 ErrRecordNotFound。
+//
+//mssql dialect 被重命名为 "github.com/jinzhu/gorm/dialects/mssql"
+//
+//Hstore 字段类型被移到专属的包里 "github.com/jinzhu/gorm/dialects/postgres"
+
+
 
 
 
