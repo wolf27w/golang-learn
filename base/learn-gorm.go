4673a255d2ced91150be4422468f29db5eaae62c
wolf <wolf27w@gmail.com>
2021-11-11 15:10:02 +0800

    gorm

--- base/learn-gorm.go
+++ base/learn-gorm.go
@@ -216,7 +216,64 @@
 //1.5.3. DeletedAt
 //对于有 DeletedAt 字段的模型，当删除它们的实例时，它们并没有被从数据库中删除，只是将 DeletedAt 字段设置为当前时间。参考 Soft Delete
 
-
+//1. 连接数据库
+//1.1. 连接数据库
+//为了连接数据库，你首先要导入数据库驱动程序。例如：
+//
+//import _ "github.com/go-sql-driver/mysql"
+//GORM 已经包含了一些驱动程序，为了方便的去记住它们的导入路径，你可以像下面这样导入 mysql 驱动程序
+//
+//import _ "github.com/jinzhu/gorm/dialects/mysql"
+//// import _ "github.com/jinzhu/gorm/dialects/postgres"
+//// import _ "github.com/jinzhu/gorm/dialects/sqlite"
+//// import _ "github.com/jinzhu/gorm/dialects/mssql"
+//1.2. 支持的数据库
+//1.2.1. MySQL
+//注意： 为了正确的处理 time.Time ，你需要包含 parseTime 作为参数。 (More supported parameters)
+//
+//import (
+//  "github.com/jinzhu/gorm"
+//  _ "github.com/jinzhu/gorm/dialects/mysql"
+//)
+//
+//func main() {
+//  db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+//  defer db.Close()
+//}
+//1.2.2. PostgreSQL
+//import (
+//  "github.com/jinzhu/gorm"
+//  _ "github.com/jinzhu/gorm/dialects/postgres"
+//)
+//
+//func main() {
+//  db, err := gorm.Open("postgres", "host=myhost port=myport user=gorm dbname=gorm password=mypassword")
+//  defer db.Close()
+//}
+//1.2.3. Sqlite3
+//import (
+//  "github.com/jinzhu/gorm"
+//  _ "github.com/jinzhu/gorm/dialects/sqlite"
+//)
+//
+//func main() {
+//  db, err := gorm.Open("sqlite3", "/tmp/gorm.db")
+//  defer db.Close()
+//}
+//1.2.4. SQL Server
+//Get started with SQL Server，它可以通过 Docker 运行在你的 Mac， Linux 上。
+//
+//import (
+//  "github.com/jinzhu/gorm"
+//  _ "github.com/jinzhu/gorm/dialects/mssql"
+//)
+//
+//func main() {
+//  db, err := gorm.Open("mssql", "sqlserver://username:password@localhost:1433?database=dbname")
+//  defer db.Close()
+//}
+//1.3. 不支持的数据库
+//GORM 官方支持以上四种数据库， 你可以为不支持的数据库编写支持，参考 GORM Dialects
 
 
 
