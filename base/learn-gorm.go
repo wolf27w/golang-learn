0820193ea4201fcbf7690c3c4a6204eba2b78884
wolf <wolf27w@gmail.com>
2021-11-11 15:19:01 +0800

    gorm

--- base/learn-gorm.go
+++ base/learn-gorm.go
@@ -1491,6 +1491,60 @@
 //  Name         string
 //}
 
+//1. 创建插件
+//GORM 本身由 Callbacks 提供支持，因此你可以根据需要完全自定义GORM。
+//
+//1.1. 注册新的 callback
+//将 callback 注册进如 callbacks：
+//
+//func updateCreated(scope *Scope) {
+//    if scope.HasColumn("Created") {
+//        scope.SetColumn("Created", NowFunc())
+//    }
+//}
+//
+//db.Callback().Create().Register("update_created_at", updateCreated)
+//// 注册 Create 进程的回调
+//1.2. 删除已有的 callback
+//从 callbacks 中删除一个 callback：
+//
+//db.Callback().Create().Remove("gorm:create")
+//// delete callback `gorm:create` from Create callbacks
+//1.3. 替换 callback
+//替换拥有相同名字的 callback ：
+//
+//db.Callback().Create().Replace("gorm:create", newCreateFunction)
+//// replace callback `gorm:create` with new function `newCreateFunction` for Create process
+//1.4. 注册 callback 的顺序
+//在注册 callbacks 时设置顺序：
+//
+//db.Callback().Create().Before("gorm:create").Register("update_created_at", updateCreated)
+//db.Callback().Create().After("gorm:create").Register("update_created_at", updateCreated)
+//db.Callback().Query().After("gorm:query").Register("my_plugin:after_query", afterQuery)
+//db.Callback().Delete().After("gorm:delete").Register("my_plugin:after_delete", afterDelete)
+//db.Callback().Update().Before("gorm:update").Register("my_plugin:before_update", beforeUpdate)
+//db.Callback().Create().Before("gorm:create").After("gorm:before_create").Register("my_plugin:before_create", beforeCreate)
+//1.5. 自带的 Callbacks
+//GORM 在处理 CRUD 操作时自带了一些 Callback，建议你在写插件前先熟悉这些 Callback：
+//
+//Create callbacks
+//
+//Update callbacks
+//
+//Query callbacks
+//
+//Delete callbacks
+//
+//Row Query callbacks - 默认没有注册的 Callbacks
+//
+//你可以用以下的方法来注册你的 Callback：
+//
+//func updateTableName(scope *gorm.Scope) {
+//  scope.Search.Table(scope.TableName() + "_draft") // append `_draft` to table name
+//}
+//
+//db.Callback().RowQuery().Register("publish:update_table_name", updateTableName)
+//请前往查看所有的 API —— https://godoc.org/github.com/jinzhu/gorm 。
 
 
 
