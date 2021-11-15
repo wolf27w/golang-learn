241fcce578505f1ffb08a184a9802624b299385e
wolf <wolf27w@gmail.com>
2021-11-15 10:26:54 +0800

    flag

--- base/learn-flag.go
+++ base/learn-flag.go
@@ -89,27 +89,35 @@
 //flag.NFlag() //返回使用的命令行参数个数
 //1.1.5. 完整示例
 //定义
-//func main() {
-//    //定义命令行参数方式1
-//    var name string
-//    var age int
-//    var married bool
-//    var delay time.Duration
-//    flag.StringVar(&name, "name", "张三", "姓名")
-//    flag.IntVar(&age, "age", 18, "年龄")
-//    flag.BoolVar(&married, "married", false, "婚否")
-//    flag.DurationVar(&delay, "d", 0, "延迟的时间间隔")
-//
-//    //解析命令行参数
-//    flag.Parse()
-//    fmt.Println(name, age, married, delay)
-//    //返回命令行参数后的其他参数
-//    fmt.Println(flag.Args())
-//    //返回命令行参数后的其他参数个数
-//    fmt.Println(flag.NArg())
-//    //返回使用的命令行参数个数
-//    fmt.Println(flag.NFlag())
-//}
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+func main() {
+   //定义命令行参数方式1
+   var name string
+   var age int
+   var married bool
+   var delay time.Duration
+   flag.StringVar(&name, "name", "张三", "姓名")
+   flag.IntVar(&age, "age", 18, "年龄")
+   flag.BoolVar(&married, "married", false, "婚否")
+   flag.DurationVar(&delay, "d", 0, "延迟的时间间隔")
+
+   //解析命令行参数
+   flag.Parse()
+   fmt.Println(name, age, married, delay)
+   //返回命令行参数后的其他参数
+   fmt.Println(flag.Args())
+   //返回命令行参数后的其他参数个数
+   fmt.Println(flag.NArg())
+   //返回使用的命令行参数个数
+   fmt.Println(flag.NFlag())
+}
 //1.1.6. 使用
 //命令行参数使用提示：
 //
