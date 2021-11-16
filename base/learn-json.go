5159943ee1f6d7f2ef3fe72fd5d1e7c6691d443c
wolf <wolf27w@gmail.com>
2021-11-16 18:27:37 +0800

    json

--- base/learn-json.go
+++ base/learn-json.go
@@ -181,77 +181,77 @@
 //1.1.4. MSGPack
 //MSGPack是二进制的json，性能更快，更省空间
 //需要安装第三方包：go get -u github.com/vmihailenco/msgpack
-//package main
-//
-//import (
-//   "fmt"
-//   "github.com/vmihailenco/msgpack"
-//   "io/ioutil"
-//   "math/rand"
-//)
-//
-//type Person struct {
-//   Name string
-//   Age  int
-//   Sex  string
-//}
-//
-//// 二进制写出
-//func writerJson(filename string) (err error) {
-//   var persons []*Person
-//   // 假数据
-//   for i := 0; i < 10; i++ {
-//      p := &Person{
-//         Name: fmt.Sprintf("name%d", i),
-//         Age:  rand.Intn(100),
-//         Sex:  "male",
-//      }
-//      persons = append(persons, p)
-//   }
-//   // 二进制json序列化
-//   data, err := msgpack.Marshal(persons)
-//   if err != nil {
-//      fmt.Println(err)
-//      return
-//   }
-//   err = ioutil.WriteFile(filename, data, 0666)
-//   if err != nil {
-//      fmt.Println(err)
-//      return
-//   }
-//   return
-//}
-//
-//// 二进制读取
-//func readJson(filename string) (err error) {
-//   var persons []*Person
-//   // 读文件
-//   data, err := ioutil.ReadFile(filename)
-//   if err != nil {
-//      fmt.Println(err)
-//      return
-//   }
-//   // 反序列化
-//   err = msgpack.Unmarshal(data, &persons)
-//   if err != nil {
-//      fmt.Println(err)
-//      return
-//   }
-//   for _, v := range persons {
-//      fmt.Printf("%#v\n", v)
-//   }
-//   return
-//}
-//
-//func main() {
-//   //err := writerJson("D:/person.dat")
-//   //if err != nil {
-//   // fmt.Println(err)
-//   // return
-//   //}
-//   err := readJson("D:/person.dat")
-//   if err != nil {
-//      fmt.Println(err)
-//      return
-//   }
-//}
+package main
+
+import (
+  "fmt"
+  "github.com/vmihailenco/msgpack"
+  "io/ioutil"
+  "math/rand"
+)
+
+type Person struct {
+  Name string
+  Age  int
+  Sex  string
+}
+
+// 二进制写出
+func writerJson(filename string) (err error) {
+  var persons []*Person
+  // 假数据
+  for i := 0; i < 10; i++ {
+     p := &Person{
+        Name: fmt.Sprintf("name%d", i),
+        Age:  rand.Intn(100),
+        Sex:  "male",
+     }
+     persons = append(persons, p)
+  }
+  // 二进制json序列化
+  data, err := msgpack.Marshal(persons)
+  if err != nil {
+     fmt.Println(err)
+     return
+  }
+  err = ioutil.WriteFile(filename, data, 0666)
+  if err != nil {
+     fmt.Println(err)
+     return
+  }
+  return
+}
+
+// 二进制读取
+func readJson(filename string) (err error) {
+  var persons []*Person
+  // 读文件
+  data, err := ioutil.ReadFile(filename)
+  if err != nil {
+     fmt.Println(err)
+     return
+  }
+  // 反序列化
+  err = msgpack.Unmarshal(data, &persons)
+  if err != nil {
+     fmt.Println(err)
+     return
+  }
+  for _, v := range persons {
+     fmt.Printf("%#v\n", v)
+  }
+  return
+}
+
+func main() {
+  //err := writerJson("D:/person.dat")
+  //if err != nil {
+  // fmt.Println(err)
+  // return
+  //}
+  err := readJson("D:/person.dat")
+  if err != nil {
+     fmt.Println(err)
+     return
+  }
+}
