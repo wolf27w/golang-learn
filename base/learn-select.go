3e8bb131c05dddaedb1ffa3547d2766b85860e40
wolf <wolf27w@gmail.com>
2021-11-18 21:54:08 +0800

    select

--- base/learn-select.go
+++ base/learn-select.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 //select 语句类似于 switch 语句，但是select会随机执行一个可运行的case。如果没有case可运行，它将阻塞，直到有case可运行
 //select 是Go中的一个控制结构，类似于用于通信的switch语句。每个case必须是一个通信操作，要么是发送要么是接收。 select 随机执行一个可运行的case。如果没有case可运行，它将阻塞，直到有case可运行。一个默认的子句应该总是可运行的。
 
@@ -72,23 +70,34 @@ import "fmt"
 
 
 
+//func main() {
+//	var c1, c2, c3 chan int
+//	var i1, i2 int
+//	select {
+//	case i1 = <-c1:
+//		fmt.Printf("received ", i1, " from c1\n")
+//	case c2 <- i2:
+//		fmt.Printf("sent ", i2, " to c2\n")
+//	case i3, ok := (<-c3):  // same as: i3, ok := <-c3
+//		if ok {
+//			fmt.Printf("received ", i3, " from c3\n")
+//		} else {
+//			fmt.Printf("c3 is closed\n")
+//		}
+//	default:
+//		fmt.Printf("no communication\n")
+//	}
+//}
+
+import (
+	"fmt"
+	"os/exec"
+)
+
 func main() {
-	var c1, c2, c3 chan int
-	var i1, i2 int
-	select {
-	case i1 = <-c1:
-		fmt.Printf("received ", i1, " from c1\n")
-	case c2 <- i2:
-		fmt.Printf("sent ", i2, " to c2\n")
-	case i3, ok := (<-c3):  // same as: i3, ok := <-c3
-		if ok {
-			fmt.Printf("received ", i3, " from c3\n")
-		} else {
-			fmt.Printf("c3 is closed\n")
-		}
-	default:
-		fmt.Printf("no communication\n")
+	c := "curl --location --request GET 'https://qa-gateway-wan.reach.store/supervision/audit/evaluate/role' --header 'app_key: evaluate' --header 'authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdW5xaSIsImV4cCI6MTYzNzgxMDMzMCwiaWF0IjoxNjM3MjA1NTMwfQ.8U5NNzGsL-FsuQ4YB-eRFupFHs_TNUmpv_s-2oOA1eABeOjbwyO8srAdXl6nBFoGX4-L2B9zWUirkwMVloRlhw'"
+	for true  {
+		cmd := exec.Command("sh","-c",c)
+		fmt.Println(cmd.Output())
 	}
 }
-
-
