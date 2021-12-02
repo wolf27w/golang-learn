f054256790b9cdb6aefb0f23d8e468a320deda16
wolf <wolf27w@gmail.com>
2021-12-02 10:30:47 +0800

    beego

--- base/learn-beego.go
+++ base/learn-beego.go
@@ -2477,7 +2477,56 @@
 //2013-08-13: 增加 Go 类型支持，包括 int8、uint8、byte、rune 等
 //2013-08-13: 增强 date／datetime 的时区支持
 
-
+//2.2. 快速入门
+//2.2.1. 简单示例
+//package main
+//
+//import (
+//    "fmt"
+//    "github.com/astaxie/beego/orm"
+//    _ "github.com/go-sql-driver/mysql" // import your used driver
+//)
+//
+//// Model Struct
+//type User struct {
+//    Id   int
+//    Name string `orm:"size(100)"`
+//}
+//
+//func init() {
+//    // set default database
+//    orm.RegisterDataBase("default", "mysql", "username:password@tcp(127.0.0.1:3306)/db_name?charset=utf8", 30)
+//
+//    // register model
+//    orm.RegisterModel(new(User))
+//
+//    // create table
+//    orm.RunSyncdb("default", false, true)
+//}
+//
+//func main() {
+//    o := orm.NewOrm()
+//
+//    user := User{Name: "slene"}
+//
+//    // insert
+//    id, err := o.Insert(&user)
+//    fmt.Printf("ID: %d, ERR: %v\n", id, err)
+//
+//    // update
+//    user.Name = "astaxie"
+//    num, err := o.Update(&user)
+//    fmt.Printf("NUM: %d, ERR: %v\n", num, err)
+//
+//    // read one
+//    u := User{Id: user.Id}
+//    err = o.Read(&u)
+//    fmt.Printf("ERR: %v\n", err)
+//
+//    // delete
+//    num, err = o.Delete(&u)
+//    fmt.Printf("NUM: %d, ERR: %v\n", num, err)
+//}
 
 
 
