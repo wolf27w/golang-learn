12af25af2407a5804497c0795a18f99bde41f4b6
wolf <wolf27w@gmail.com>
2021-12-02 10:18:42 +0800

    beego

--- base/learn-beego.go
+++ base/learn-beego.go
@@ -1885,5 +1885,46 @@
 //1.1. 特别注意点
 //因为 session 内部采用了 gob 来注册存储的对象，例如 struct，所以如果你采用了非 memory 的引擎，请自己在 main.go 的 init 里面注册需要保存的这些结构体，不然会引起应用重启之后出现无法解析的错误
 
-
+//1. 过滤器
+//beego 支持自定义过滤中间件，例如安全验证，强制跳转等。
+//
+//过滤器函数如下所示：
+//
+//beego.InsertFilter(pattern string, position int, filter FilterFunc, params ...bool)
+//InsertFilter 函数的三个必填参数，一个可选参数
+//
+//pattern 路由规则，可以根据一定的规则进行路由，如果你全匹配可以用 *
+//position 执行 Filter 的地方，五个固定参数如下，分别表示不同的执行过程
+//BeforeStatic 静态地址之前
+//BeforeRouter 寻找路由之前
+//BeforeExec 找到路由之后，开始执行相应的 Controller 之前
+//AfterExec 执行完 Controller 逻辑之后执行的过滤器
+//FinishRouter 执行完逻辑之后执行的过滤器
+//filter filter 函数 type FilterFunc func(*context.Context)
+//params
+//设置 returnOnOutput 的值(默认 true), 如果在进行到此过滤之前已经有输出，是否不再继续执行此过滤器,默认设置为如果前面已有输出(参数为true)，则不再执行此过滤器
+//是否重置 filters 的参数，默认是 false，因为在 filters 的 pattern 和本身的路由的 pattern 冲突的时候，可以把 filters 的参数重置，这样可以保证在后续的逻辑中获取到正确的参数，例如设置了 /api/* 的 filter，同时又设置了 /api/docs/* 的 router，那么在访问 /api/docs/swagger/abc.js 的时候，在执行 filters 的时候设置 :splat 参数为 docs/swagger/abc.js，但是如果不清楚 filter 的这个路由参数，就会在执行路由逻辑的时候保持 docs/swagger/abc.js，如果设置了 true，就会重置 :splat 参数.
+//AddFilter 从beego1.3 版本开始已经废除
+//
+//如下例子所示，验证用户是否已经登录，应用于全部的请求：
+//
+//var FilterUser = func(ctx *context.Context) {
+//    _, ok := ctx.Input.Session("uid").(int)
+//    if !ok && ctx.Request.RequestURI != "/login" {
+//        ctx.Redirect(302, "/login")
+//    }
+//}
+//
+//beego.InsertFilter("/*",beego.BeforeRouter,FilterUser)
+//这里需要特别注意使用 session 的 Filter 必须在 BeforeStatic 之后才能获取，因为 session 没有在这之前初始化。
+//
+//还可以通过正则路由进行过滤，如果匹配参数就执行：
+//
+//var FilterUser = func(ctx *context.Context) {
+//    _, ok := ctx.Input.Session("uid").(int)
+//    if !ok {
+//        ctx.Redirect(302, "/login")
+//    }
+//}
+//beego.InsertFilter("/user/:id([0-9]+)",beego.BeforeRouter,FilterUser)
 
