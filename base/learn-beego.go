90b3edead3fb03b6f3329dd6915d5f3915176b16
wolf <wolf27w@gmail.com>
2021-12-03 18:29:37 +0800

    beego

--- base/learn-beego.go
+++ base/learn-beego.go
@@ -3753,7 +3753,20 @@
 //}
 
 
-
+//1. 事务处理
+//ORM 可以简单的进行事务操作
+//
+//o := NewOrm()
+//err := o.Begin()
+//// 事务处理过程
+//...
+//...
+//// 此过程中的所有使用 o Ormer 对象的查询都在事务处理范围内
+//if SomeError {
+//    err = o.Rollback()
+//} else {
+//    err = o.Commit()
+//}
 
 
 
