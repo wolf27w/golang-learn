725d6e4e2b36598f19a4789328d078a111eeb496
wolf <wolf27w@gmail.com>
2022-03-18 16:24:05 +0800

    learn

--- base/learn-concurrency.go
+++ base/learn-concurrency.go
@@ -806,8 +806,6 @@
 
 package main
 
-import "fmt"
-
 //func test1(ch chan string)  {
 //	time.Sleep(time.Second * 5)
 //	ch <- "test1"
@@ -1509,34 +1507,7 @@ import "fmt"
 
 
 
-func main() {
-	values := []int{4, 93, 84, 85, 80, 37, 81, 93, 27,12}
-	fmt.Println(values)
-	BubbleAsort(values)
-	BubbleZsort(values)
-}
 
-func BubbleAsort(values []int) {
-	for i := 0; i < len(values)-1; i++ {
-		for j := i+1; j < len(values); j++ {
-			if  values[i]>values[j]{
-				values[i],values[j] = values[j],values[i]
-			}
-		}
-	}
-	fmt.Println(values)
-}
-
-func BubbleZsort(values []int) {
-	for i := 0; i < len(values)-1; i++ {
-		for j := i+1; j < len(values); j++ {
-			if  values[i]<values[j]{
-				values[i],values[j] = values[j],values[i]
-			}
-		}
-	}
-	fmt.Println(values)
-}
 
 
 
