0ae572eae6965ae3b7c1d04f625031c6bbae4b61
zhangpeng <wolf27w@gmail.com>
2022-04-18 16:30:06 +0800

    crawler

--- base/learn-crawler.go
+++ base/learn-crawler.go
@@ -1,17 +1,39 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-)
-
 func main()  {
 	//fmt.Println(os.Getwd())
-	path := "golang-learn/README.md"
-	content, err := ioutil.ReadFile(path)
-	if err != nil {
-		fmt.Println("文件错误",err)
-	}
-	fmt.Printf("%v",content)
+	path := "golang-learn/index.html"
+	//content, err := os.OpenFile(path,os.O_WRONLY | os.O_CREATE, 0655)
+	//if err != nil {
+	//	fmt.Println("文件错误",err)
+	//	return
+	//}
+	//defer content.Close()
+	//str := "hello golang \r\n"
+	//write := bufio.NewWriter(content)
+	////for i := 0; i < 5; i++ {
+	////	write.WriteString(str)
+	////}
+	//write.WriteString(str)
+	//write.Flush()//刷新缓存写入到文件中
 
+	//con, err := os.Open(path)
+	//if err != nil {
+	//	fmt.Print("文件读取错误")
+	//}
+	//defer con.Close()
+	//reader := bufio.NewReader(con)
+	//for {
+	//	strs,err := reader.ReadString('\n')
+	//	if err == io.EOF {
+	//		fmt.Print(err)
+	//		break
+	//	}
+	//	fmt.Print(strs)
+	//}
+	//fmt.Println("文件结束")
+
+
+
+	
 }
