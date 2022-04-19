4e98e09ca749dd213ae7ae4e0dedcd5b82fa9d84
zhangpeng <wolf27w@gmail.com>
2022-04-19 17:54:50 +0800

    crawler

--- base/learn-crawler.go
+++ base/learn-crawler.go
@@ -8,16 +8,25 @@ import (
 
 func main()  {
 	//fmt.Println(os.Getwd())
-	path := "golang-learn/index5.html"
-	//content, err := os.OpenFile(path,os.O_WRONLY | os.O_TRUNC, 0655)
-	content, err := os.OpenFile(path,os.O_RDWR | os.O_TRUNC | os.O_CREATE,00)
+	path := "golang-learn/index.html"
+	file,err := os.Open(path)
 	if err != nil {
-		fmt.Println("文件错误",err)
-		return
+		fmt.Println("file filed err %v",err)
 	}
-	defer content.Close()
-	str := "hello golang \r\n"
-	write := bufio.NewWriter(content)
-	write.WriteString(str)
-	write.Flush()//刷新缓存写入到文件中
+	defer file.Close()
+
+	//content, err := os.OpenFile(path,os.O_WRONLY | os.O_TRUNC, 0655)
+	////content, err := os.OpenFile(path,os.O_RDWR | os.O_TRUNC | os.O_CREATE, 0655)//两者都有覆盖的意思
+	//if err != nil {
+	//	fmt.Println("文件错误",err)
+	//	return
+	//}
+	//defer content.Close()
+	//str1 := "hello www.wulaoer.org"
+	read := bufio.NewReader(file)
+	fmt.Print(read)
+
+
+
+
 }
