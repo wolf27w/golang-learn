dc6f588c1805b35a5ff219e64827e6657aace328
zhangpeng <wolf27w@gmail.com>
2022-04-19 18:01:27 +0800

    crawler

--- base/learn-crawler.go
+++ base/learn-crawler.go
@@ -1,30 +1,32 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+	"io"
+	"log"
 	"os"
 )
+
+
+func appendStringInFile(filePath, content string) {
+	file, err := os.OpenFile(filePath, os.O_RDONLY|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("文件打开失败: %v", err)
+	}
+	defer file.Close()
+	// 查找文件末尾的偏移量
+	n, _ := file.Seek(2, io.SeekStart)
+	// 从末尾的偏移量开始写入内容
+	_, err = file.WriteAt([]byte("\n"+content), n)
+	if err != nil {
+		log.Fatalf("文件写入失败: %v", err)
+	}
+}
 
 func main()  {
 	//fmt.Println(os.Getwd())
 	path := "golang-learn/index.html"
-	file,err := os.Open(path)
-	if err != nil {
-		fmt.Println("file filed err %v",err)
-	}
-	defer file.Close()
-
-	//content, err := os.OpenFile(path,os.O_WRONLY | os.O_TRUNC, 0655)
-	////content, err := os.OpenFile(path,os.O_RDWR | os.O_TRUNC | os.O_CREATE, 0655)//两者都有覆盖的意思
-	//if err != nil {
-	//	fmt.Println("文件错误",err)
-	//	return
-	//}
-	//defer content.Close()
-	//str1 := "hello www.wulaoer.org"
-	read := bufio.NewReader(file)
-	fmt.Print(read)
+	str1 := "hello www.wulaoer.org"
+	appendStringInFile(path,str1)
 
 
 
