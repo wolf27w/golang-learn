e8413ed241eab9523cc5fc6f2252f1ee5996a577
zhangpeng <wolf27w@gmail.com>
2022-04-19 18:31:53 +0800

    crawler

--- base/learn-crawler.go
+++ base/learn-crawler.go
@@ -1,34 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"io"
-	"log"
 	"os"
 )
 
 
-func appendStringInFile(filePath, content string) {
-	file, err := os.OpenFile(filePath, os.O_RDONLY|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatalf("文件打开失败: %v", err)
-	}
-	defer file.Close()
-	// 查找文件末尾的偏移量
-	n, _ := file.Seek(2, io.SeekStart)
-	// 从末尾的偏移量开始写入内容
-	_, err = file.WriteAt([]byte("\n"+content), n)
-	if err != nil {
-		log.Fatalf("文件写入失败: %v", err)
-	}
-}
-
-func main()  {
+func main() {
 	//fmt.Println(os.Getwd())
 	path := "golang-learn/index.html"
-	str1 := "hello www.wulaoer.org"
-	appendStringInFile(path,str1)
-
-
-
+	//path1 := "golang-learn/index1.html"
+	srcfile,err := os.OpenFile(path,os.O_RDWR,0644)
+	if err != nil {
+		fmt.Println("open file filed %v",err)
+		return
+	}
+	tmpfile,err := os.OpenFile("./123.tmp",os.O_CREATE | os.O_WRONLY | os.O_TRUNC,0644)
+	if err != nil {
+		fmt.Printf("open file filed %v",err)
+		return
+	}
+	var b [10]byte //定义文件的需要插入的定位符
+	n,err := srcfile.Read(b[:])
+	if err != nil {
+		fmt.Printf("read file failed,err:%v\n",err)
+		return
+	}
+	tmpfile.Write(b[:n])
+	tmpfile.WriteString("www.wulaoer.org")
+	var x [1024]byte
+	for {
+		n,err := srcfile.Read(x[:])
+		if err == io.EOF {
+			tmpfile.Write(x[:n])
+			break
+		}
+		if err != nil {
+			fmt.Printf("read from file failed,err:%v\n",err)
+			return
+		}
+		tmpfile.Write(x[:n])
+	}
+	tmpfile.Close()
+	srcfile.Close()
+	ok := os.Rename("./123.tmp",path)
+	if ok == nil {
+		fmt.Printf(" sucnss")
+	} else {
+		fmt.Printf("error")
+	}
 
 }
