95374a80e3c5aa15b73a45c9315a4b170f242c12
zhangpeng <wolf27w@gmail.com>
2022-04-20 19:02:02 +0800

    crawler

--- base/go.mod
+++ base/go.mod
@@ -3,9 +3,18 @@ module base
 go 1.15
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/antchfx/htmlquery v1.2.4 // indirect
+	github.com/antchfx/xmlquery v1.3.10 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/gomodule/redigo v1.8.5 // indirect
 	github.com/jackdanger/collectlinks v0.0.0-20160421202702-24c4ee2870ba // indirect
 	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.0.0-20220418201149-a630d4f3e7a2 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 )
--- base/learn-crawler.go
+++ base/learn-crawler.go
@@ -2,40 +2,43 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"regexp"
+	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/debug"
 )
-
-var (
-	reLineke = `&nbsp;&nbsp;&nbsp;&nbsp;[\u4e00-\u9fa5]`
-)
-
-
-// 处理异常
-func HandleError(err error, why string) {
-	if err != nil {
-		fmt.Println(why, err)
-	}
-}
-
-func contente(url string)  {
-	resp,err := http.Get(url)
-	HandleError(err,"content error")
-	defer resp.Body.Close()
-	page,err := ioutil.ReadAll(resp.Body)
-	HandleError(err,"iou")
-	pages := string(page)
-	re := regexp.MustCompile(reLineke)
-	results := re.FindAllStringSubmatch(pages,-1)
-	for _, resylt := range results {
-		fmt.Println(resylt)
-	}
-}
-
-
 
 func main() {
-	path:="https://www.qbiqu.com/0_4/4235911.html"
-	contente(path)
+	c := colly.NewCollector(colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"), colly.MaxDepth(1), colly.Debugger(&debug.LogDebugger{}))
+	//文章列表
+	//c.OnHTML("div[class='dahengfu']", func(e *colly.HTMLElement) {
+	//	//列表中每一项
+	//	e.ForEach("div", func(i int, item *colly.HTMLElement) {
+	//		//文章链接
+	//		href := item.ChildAttr("a[href='/'] > a[class='title']", "href")
+	//		//文章标题
+	//		title := item.ChildText("div[class='bookname'] > a[class='title']")
+	//		//文章摘要
+	//		summary := item.ChildText("div[id='content'] > p[class='abstract']")
+	//		fmt.Println(title, href)
+	//		fmt.Println(summary)
+	//		//fmt.Println()
+	//	})
+	//})
+	c.OnHTML("div[class='box_con']", func(e *colly.HTMLELement) {
+		e.FOREACH("li", func(i int, item *colly.HTMLElement) {
+			href := item.ChildAttr("div[class='nav']" > a[class='header'],)
+		})
+	})
+	err := c.Visit("https://www.qbiqu.com/7_7351/14281109.html")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
+
+
+
+
+
+//func main() {
+//	path:="https://www.qbiqu.com/0_4/4235911.html"
+//	contente(path)
+//}
